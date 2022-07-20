1d9898a7c19376530817b6e08e0194035d8141e8
franco <nahuelgodina@gmail.com>
2022-07-20 14:47:13 -0300

    Create user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema(
+    {
+        username: {type: String, required:true, maxLength: 20},
+        password: {type: String, required: true},
+        member: {type: Boolean, default: false}
+    }
+);
+
+// Export model
+module.exports = mongoose.model("User", UserSchema);
