8417b5fe44885f3323844a14d4b860fbfb7315cb
franco <nahuelgodina@gmail.com>
2022-07-21 11:42:05 -0300

    Show messages in index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,9 @@
+const Message = require("../models/message");
+
 exports.index = async (req, res, next) => {
     try {
-        return res.render("index", { title: "AnonPosting", user: req.user })
+        const messages = await Message.find().sort([["timestamp", "descending"]]).populate("username")
+        return res.render("index", { title: "AnonPosting", user: req.user, messages: messages })
     }
     catch(err) {
         return next(err)
