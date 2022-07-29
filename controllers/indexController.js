7bf3c8352d0200b686001927d027f7fbe01eee10
franco <nahuelgodina@gmail.com>
2022-07-29 13:30:40 -0300

    Rename page title

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const Message = require("../models/message");
 exports.index = async (req, res, next) => {
     try {
         const messages = await Message.find().sort([["timestamp", "descending"]]).populate("username")
-        return res.render("index", { title: "Hunter club", user: req.user, messages: messages })
+        return res.render("index", { title: "Members only", user: req.user, messages: messages })
     }
     catch(err) {
         return next(err)
